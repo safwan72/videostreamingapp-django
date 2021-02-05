09f169001d7725d0e912071dca100287d6e5be8d
safwan <safwanali137@gmail.com>
2021-02-06 01:07:26 +0600

    second commit

--- VideoStream/settings.py
+++ VideoStream/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +24,10 @@ MEDIA_DIR=BASE_DIR/'media'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zl(y(9!2dqdu5r-zh8gj*!88y68$78vnl%h0oal!c0^hdy=1ca'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
